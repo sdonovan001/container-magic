e1a94fe7833cdb0d15eca2f09b558ce31d121875
sdonovan001 <scott@the-donovans.org>
2025-08-31 15:30:23 +0000

    adding more clarity to script

--- src/docker-exec.sh
+++ src/docker-exec.sh
@@ -2,16 +2,16 @@
 
 usage() {
    echo ""
-   echo "Usage: $0 <CONTAINER_ID> <CMD>"
-   echo "Description:    This script allows you to execute commands inside "
-   echo "                a running container."
+   echo "Usage: $0 <container_id> <cmd>"
+   echo "Description:    This script allows you to execute commands inside a"
+   echo "                running container."
    echo ""
    echo "Options:"
    echo "  -h            Display this usage message and exit."
    echo ""
    echo "Arguments:"
-   echo "  CONTAINER_ID: Unique identifier of the running container.  Can"
-   echo "                be found by running ./docker-ps.sh."
+   echo "  CONTAINER_ID: Unique identifier of the running container.  Can be"
+   echo "                found by running ./docker-ps.sh."
    echo "  CMD:          Command to execute inside the running container."
    echo ""
 }
@@ -30,7 +30,10 @@ then
    exit 1
 fi
 
+# PID of unshare command
 ns_pid=$(ps -aef|grep $1|grep unshare|awk '{ print $2 }')
+# PID of container entrypoint
 child_pid=$(ps --ppid ${ns_pid}|grep -v PID|awk '{ print $1 }')
 
+# We want to enter the same namespace as the entrypoint
 nsenter -a -t ${child_pid} -w -r /bin/bash -c "mount -t proc proc /proc; $2"
