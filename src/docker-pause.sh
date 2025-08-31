d2c948a850614a460a08dcea6e07193c0bb161fb
sdonovan001 <scott@the-donovans.org>
2025-08-31 15:34:38 +0000

    cleaning up

--- src/docker-pause.sh
+++ src/docker-pause.sh
@@ -2,15 +2,15 @@
 
 usage() {
    echo ""
-   echo "Usage: $0 <CONTAINER_ID>"
+   echo "Usage: $0 <container_id>"
    echo "Description:    This script pauses a running container causing all"
-   echo "                all processes inside the container to be suspended."
+   echo "                processes inside the container to be suspended."
    echo ""
    echo "Options:"
    echo "  -h            Display this usage message and exit."
    echo ""
    echo "Arguments:"
-   echo "  CONTAINER_ID: Unique identifier of the running container.  Can"
+   echo "  container_id: Unique identifier of the running container.  Can"
    echo "                be found by running ./docker-ps.sh."
    echo ""
 }
@@ -31,4 +31,6 @@ fi
 
 container_id=$1
 cgroup_root="/sys/fs/cgroup"
+
+# Freeze all processes running in this cgroup
 echo "1" > "${cgroup_root}/${container_id}/cgroup.freeze"
